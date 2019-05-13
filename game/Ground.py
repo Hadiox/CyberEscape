7fe6f7489ca067ba0f6d7e4e2e934176e5c11f0f
Bartek Łagosz <lagosz.bartlomiej@gmail.com>
2019-05-13 14:02:13 +0100

    changes

---
+++ game/Ground.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+
+class Ground(object):
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.id = 0
+        self.height = height
+        self.hitbox = (x, y, width, height)
+        self.img = pygame.image.load(os.path.join('resources/Ground', 'simple_ground.jpg'))
+
+    def draw(self, window):
+        self.hitbox = (self.x + 25, self.y + 3, self.width, self.height)
+        window.blit(pygame.transform.scale(self.img, (self.width, self.height)), (self.x, self.y))
+        #pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
---
+++ game/GroundGenerator.py
@@ -0,0 +1,24 @@
+import threading
+import time
+from game.Ground import *
+from game.Obstacles import *
+import random
+
+
+class GroundGenerator(threading.Thread):
+    def __init__(self, objects, screen_height):
+        threading.Thread.__init__(self)
+        self.objects = objects
+        self.screen_height = screen_height
+        self.obstacle = 0
+
+    def run(self):
+        while (True):
+            if self.obstacle == 1:
+                obs_height = random.randrange(50, 200)
+                obs_width = random.randrange(50, 200)
+                self.objects.append(Rectangle(self.screen_height + 20, 598 - obs_height, obs_width,
+                                              obs_height))
+                self.obstacle = 0
+            self.objects.append(Ground(self.screen_height + 20, 598, 400, 180))
+            time.sleep(0.500)
