b898a6b86c286b5672ef2c45b5ed66c306c304d8
Bartek Łagosz <lagosz.bartlomiej@gmail.com>
2019-06-02 20:55:47 +0100

    Added hand detection

---
+++ opencv/detection_utils.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import imutils
+import numpy as np
+
+background = None
+
+def find_run_avg(image, accWeight):
+    global background
+    if background is None:
+        background = image.copy().astype("float")
+        return
+    cv.accumulateWeighted(image,background,accWeight)
+
+
+def segment(image,threshold = 25):
+    global background
+    diff = cv.absdiff(background.astype("uint8"),image)
+    thresholded = cv.threshold(diff,threshold,255,cv.THRESH_BINARY)[1]
+    (_,cnts,_) = cv.findContours(thresholded.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+    if len(cnts) == 0:
+        return
+    else:
+        segmented = max(cnts,key=cv.contourArea)
+        return(thresholded,segmented)
+
